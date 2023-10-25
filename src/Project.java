f471d7121081a657c6b94be6a51ce267d74bd186
avascannell <ava_scannell26@milton.edu>
2023-10-25 08:16:54 -0400

    complted my project

--- src/Project.java
+++ src/Project.java
@@ -1,54 +1,89 @@
 public class Project extends World {
-    public void go() {
-        plane.trailWidth=15;
-        plane.setColor(255, 0, 0);
-      clam();
-      plane.teleport(387,717);
-      plane.setColor(255,0,0);
-      clam();
+
+        public int red=100;
+        public int blue=0;
+        public int green=0;
+        public int row;
+
+        public void go() {
+
+
+                if(red < 255 && green < 255 && blue < 255){
+                       red=255;
+                       green=255;
+                       blue=255;
+                        plane.setPixelColor(red, green, blue);
+
+                }
+                plane.trailWidth = 10;
+                clam();
+        }
+                public void clam() {
+                        plane.isTrail = false;
+                        plane.pausetime = 0;
+                        plane.turn(430);
+                        plane.move(200);
+                        plane.turn(400);
+                        plane.move(200);
+                        plane.isTrail = true;
+                        plane.circle(.005);
+                        plane.isTrail = false;
+                        plane.turn(90);
+                        plane.move(155);
+                        plane.isTrail = true;
+                        plane.circle(.005);
+                        plane.isTrail = false;
+                        plane.turn(35);
+                        plane.move(148);
+                        plane.isTrail = true;
+                        plane.circle(.005);
+                        plane.isTrail = false;
+                        plane.turn(55);
+                        plane.move(157);
+                        plane.isTrail = true;
+                        plane.circle(.005);
+                        plane.isTrail = false;
+                        plane.turn(25);
+                        plane.move(139);
+                        plane.isTrail = true;
+                        plane.circle(.005);
+                        plane.isTrail = false;
+                        plane.turn(100);
+                        plane.move(113);
+                        plane.isTrail = true;
+                        plane.move(160);
+                        plane.turn(70);
+                        plane.move(160);
+
+                        red = plane.howMuchRed();
+                        green = plane.howMuchGreen();
+                        blue = plane.howMuchBlue();
+                        plane.teleport(-15,488);
+                        plane.trailWidth=40;
+                        for(int x=0;x<10;x++) {
+                         for(int y=0;y<2;y++){
+                                        plane.startingAngle(45);
+                                        plane.move(100);
+                                        plane.turn(90);
+                                        plane.move(100);
+                                        plane.turn(270);
+                                }
+                         plane.trailWidth=plane.trailWidth-5;
+                        }
+                        }
 
 
 
 
-    }
-
-    public void clam(){
-        plane.isTrail= false;
-        plane.pausetime= 0;
-        plane.turn(430);
-        plane.move(200);
-        plane.turn(400);
-        plane.move(200);
-        plane.isTrail=true;
-        plane.circle(.005);
-        plane.isTrail= false;
-        plane.turn(90);
-        plane.move(155);
-        plane.isTrail=true;
-        plane.circle(.005);
-        plane.isTrail=false;
-        plane.turn(35);
-        plane.move(148);
-        plane.isTrail=true;
-        plane.circle(.005);
-        plane.isTrail=false;
-        plane.turn(55);
-        plane.move(157);
-        plane.isTrail=true;
-        plane.circle(.005);
-       plane.isTrail=false;
-       plane.turn(25);
-       plane.move(139);
-       plane.isTrail= true;
-       plane.circle(.005);
-       plane.isTrail=false;
-       plane.turn(100);
-       plane.move(113);
-       plane.isTrail=true;
-       plane.move(160);
-       plane.turn(70);
-       plane.move(160);
 
 
-    }
-}
+
+
+                }
+
+
+
+
+
+
+
